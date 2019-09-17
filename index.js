849df8a48b19bc7bb7234345750b773e14462b16
Jackson Asprilla <yayokalonji@icloud.com>
2019-09-17 14:39:07 -0500

    Change of Json and add eslint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+'use strict';
 const express = require("express");
-const fs = require('fs');
-path = require('path');
-bodyParser = require('body-parser');
+let jsonData = require('./RS_analyzeMLP.json');
+const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/json' }));
@@ -11,8 +11,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/orchestrator/analize', function(req, res) {
-    console.log(req.body);  
-    res.json(JSON.parse('{"env:Envelope": {   "env:Body": {"analyzeReturn": {"identificationData": { "orgName": "BC_EMP_CashManagement","sessionId": "4lIBkIamaMcl0r9LZmgOcK35kDw=","transactionId": "etdo2.davivienda.842018102216153987394482846508"},"messageHeader": {"requestId": "7769701","requestType": "ANALYZE","timeStamp": "20181022081547","version": "6.5"},"statusHeader": { "statusCode": "200" },"riskResult": {"riskScore": "2","deviceIdentificationAssurance": "NONE"},"collectableCredentialList":[{"credentialType": "ISP","credentialSpecificType": "Valor1" },{"credentialType": "COUNTRY","credentialSpecificType": "RECENT"},{"credentialType": "CITY","credentialSpecificType": "NO"},{"credentialType": "REAL_ISP","credentialSpecificType": "valor2"},{"credentialType": "DECIMAL_IP","credentialSpecificType": "125467522"}]}}}}'));
+    res.status(200).json(jsonData);
 });
 
 app.get('/orchestrator/markcase', function(req, res) {
